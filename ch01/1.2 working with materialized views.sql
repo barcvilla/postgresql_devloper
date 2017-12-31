024e2e7e5bd49df8a5de543a59c041ef1618eddf
Carlos Villanueva <ceva_19@hotmail.com>
2017-12-31 17:03:29 -0300

    Working with Materialized Views

---
+++ ch01/1.2 working with materialized views.sql
@@ -0,0 +1,7 @@
+﻿-- Vistas Materializadas ---
+-- Vista materailizada de solo lectura.
+create materialized view suppliers_matview as select * from suppliers;
+
+-- Vista Materializable Actualizable
+create materialized view suppliers_matview for update 
+as select * from suppliers;
