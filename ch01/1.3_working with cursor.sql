afae126493e9a428b06d9781f732572d2cf99c4a
Carlos Villanueva <ceva_19@hotmail.com>
2018-02-27 23:49:42 -0300

    Working with Cursor

---
+++ ch01/1.3_working with cursor.sql
@@ -0,0 +1,14 @@
+﻿/* declaramos un cursor */
+BEGIN;
+DECLARE order_cur CURSOR
+FOR SELECT * FROM orders;
+
+/* Usamos un cursor */
+FETCH FROM order_cur;
+
+/* Recuperamos las 4 primeras filas almacenadas en el resultset. 
+   La direccion no es necesaria indicarla, FORWARD es por defecto. */
+FETCH 4 FROM order_cur;
+
+/* Cerrar Cursor */
+CLOSE order_cur;
